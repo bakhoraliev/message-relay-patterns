6a9aa7beb92a6b57340584267896264fef6cbb10
Ilkhomidin Bakhoraliev <bakhoraliev.ilkhomidin@proton.me>
2025-07-25 16:31:56 +0300

    Add CREATE DDL for outbox table

---
+++ outbox.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS general;
+CREATE TABLE IF NOT EXISTS general.outbox (
+    id SERIAL PRIMARY KEY,
+    topic VARCHAR(255) NOT NULL,
+    key VARCHAR(255),
+    value BYTEA NOT NULL,
+    headers JSONB DEFAULT NULL,
+    partition INT DEFAULT NULL,
+    processed BOOLEAN DEFAULT FALSE
+);
