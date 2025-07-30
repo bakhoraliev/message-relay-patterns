b5cd56978038f6c071aa5ac90888f5325cdf971e
Ilkhomidin Bakhoraliev <bakhoraliev.ilkhomidin@proton.me>
2025-07-31 02:26:58 +0300

    Impore transaction log tailing implementation

--- transaction_log_tailing.py
+++ transaction_log_tailing.py
@@ -1,11 +1,18 @@
 import json
 import logging
 import os
-from typing import Callable, TypedDict
+from typing import (
+    Callable,
+    TypedDict,
+)
 
 import kafka
 import psycopg2
-from psycopg2.extras import LogicalReplicationConnection, ReplicationMessage
+from psycopg2.extras import (
+    LogicalReplicationConnection,
+    ReplicationCursor,
+    ReplicationMessage,
+)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -26,38 +33,82 @@ class OutboxMessage(TypedDict):
     partition: int | None
 
 
-def create_transaction_log_consumer(
-    producer: kafka.KafkaProducer,
-) -> TransactionLogConsumer:
+def publish_message(producer: kafka.KafkaProducer, message: OutboxMessage):
+    logger.info(
+        "Publishing message id=%d to topic='%s', partition=%s.",
+        message["id"],
+        message["topic"],
+        message["partition"],
+    )
+    producer.send(
+        topic=message["topic"],
+        key=message["key"],
+        value=message["value"],
+        headers=message["headers"],
+        partition=message["partition"],
+    )
+    producer.flush()  # Ensure the message is sent immediately
+    logger.debug("Message id=%d published and flushed.", message["id"])
 
-    def consume(replication_message: ReplicationMessage):
-        replication_message_payload = json.loads(
-            replication_message.data.decode("utf-8")
-        )
-        outbox_message = OutboxMessage(
-            id=replication_message_payload["id"],
-            topic=replication_message_payload["topic"],
-            key=replication_message_payload["key"],
-            value=replication_message_payload["value"],
-            headers=replication_message_payload["headers"],
-            partition=replication_message_payload["partition"],
-        )
-        producer.send(
-            topic=outbox_message["topic"],
-            key=outbox_message["key"],
-            value=outbox_message["value"],
-            headers=outbox_message["headers"],
-            partition=outbox_message["partition"],
-        )
-        producer.flush()
-        replication_message.cursor.send_feedback(
-            flush_lsn=replication_message.data_start
-        )
 
-    return consume
+def parse_replication_message(
+    replication_message: ReplicationMessage,
+) -> list[OutboxMessage]:
+    logger.info("Parsing replication message: %s", replication_message.payload)
+    parsed_payload = json.loads(
+        replication_message.payload
+    )  # Assume wal2json plugin is used
+    outbox_messages = []
+    for change in parsed_payload.get("changes", []):
+        if (
+            change["kind"] == "insert"
+            and change["schema"] == "general"
+            and change["table"] == "outbox"
+        ):
+            outbox_messages.append(
+                OutboxMessage(
+                    **dict(
+                        zip(
+                            change["columnnames"],
+                            change["columnvalues"],
+                        )
+                    )
+                )
+            )
+    return outbox_messages
+
+
+def transaction_log_tailing(
+    replication_slot: str, cursor: ReplicationCursor, producer: kafka.KafkaProducer
+):
+    """
+    Implements the Transaction Log Tailing pattern.
+
+    Starts a logical replication stream from the specified replication slot,
+    and consumes changes from the database. Consumer function processes each
+    replication message by parsing it, selecting only `insert` changes, and publishing
+    them to Kafka one by one. Acknowledgment is sent back to the database after each
+    message is processed.
+    """
+
+    def consumer(replication_message: ReplicationMessage):
+        logger.debug("Consuming replication message: %s", replication_message.payload)
+        outbox_messages = parse_replication_message(replication_message)
+        for outbox_message in outbox_messages:
+            publish_message(producer, outbox_message)
+        cursor.send_feedback(
+            flush_lsn=replication_message.data_start,
+            reply=True,
+        )
+        logger.debug("Sent feedback for replication message: %s", replication_message)
+
+    cursor.start_replication(slot_name=replication_slot, decode=True)
+    cursor.consume_stream(consumer)
 
 
 def main():
+    logger.info("Starting transaction log tailing.")
+    replication_slot_name = os.getenv("REPLICATION_SLOT_NAME", "outbox")
     db = psycopg2.connect(
         dsn=os.getenv(
             "DATABASE_DSN",
@@ -76,10 +127,7 @@ def main():
 
     try:
         with db.cursor() as cursor:
-            cursor.start_replication(slot_name=..., decode=True)
-            cursor.consume_stream(
-                create_transaction_log_consumer(producer),
-            )
+            transaction_log_tailing(replication_slot_name, cursor, producer)
     except KeyboardInterrupt:
         logger.info("Transaction log tailing interrupted by user. Shutting down.")
     except Exception as e:
@@ -87,6 +135,7 @@ def main():
     finally:
         db.close()
         producer.close()
+        logger.info("Database and Kafka producer connections closed. Exiting.")
 
 
 if __name__ == "__main__":
