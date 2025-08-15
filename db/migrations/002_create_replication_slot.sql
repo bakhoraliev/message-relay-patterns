c3b3612f25c7fe3224e3b532c4e3b28d0564c8f5
Ilkhomidin Bakhoraliev <bakhoraliev.ilkhomidin@proton.me>
2025-08-15 21:27:03 +0300

    Use EXECUTE instead of PERFORM

--- db/migrations/002_create_replication_slot.sql
+++ db/migrations/002_create_replication_slot.sql
@@ -3,7 +3,7 @@ BEGIN
     IF NOT EXISTS (
         SELECT 1 FROM pg_replication_slots WHERE slot_name = 'outbox'
     ) THEN
-        PERFORM pg_create_logical_replication_slot('outbox', 'wal2json');
+        EXECUTE 'SELECT pg_create_logical_replication_slot(''outbox'', ''wal2json'')';
     END IF;
 END
 $$;
